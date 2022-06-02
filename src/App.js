698dcb7fb15358b97f0f5aedfacc8c7214f82812
smartobi <agbawochinonso488@gmail.com>
2022-06-02 06:19:08 +0100

    little adjustment

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p>DevOps Class</p>
         <p>Bye there!</p>
         <a
           className="App-link"
